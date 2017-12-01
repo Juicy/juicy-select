2d450a4ac7bd52ff07d7a42a7a6ef86c7d5d93d2
Omar Alshaker <omar@omaralshaker.com>
2017-12-01 23:34:28 +0100

    Modernize event simulation and drop for...of

--- test/helpers.js
+++ test/helpers.js
@@ -1,7 +1,4 @@
 function simulateChangeEvent(element) {
-  if ('createEvent' in document) {
-    var evt = document.createEvent('HTMLEvents');
-    evt.initEvent('change', false, true);
-    element.dispatchEvent(evt);
-  } else element.fireEvent('onchange');
+  var evt = new Event('change', { bubbles: true, composed: true });
+  element.dispatchEvent(evt);
 }
